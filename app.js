1ed27c10da41b0b40a3e51751601163d07ca3d51
Raivydas <raivydas.ceslauskas@gmail.com>
2020-02-17 20:18:32 +0200

    Edit project structure, add navbar to homepage

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
 const mongoose = require ('mongoose');
-
-
-mongoose.connect('mongodb://localhost:27017/bidit', {useNewUrlParser: true, useUnifiedTopology: true});
-
+const loginPage = require('./routes/login');
+const home = require('./routes/home');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/bidit', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//app.use(express.json());
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
-})
-
+app.use('/login', loginPage);
+app.use('/', home);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
