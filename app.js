726f4e782fa31ef4b2423a18698856e8d1b95798
Raivydas <raivydas.ceslauskas@gmail.com>
2020-02-19 19:10:34 +0200

    Add User registration to DB functionality

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const express = require ('express');
 const bodyParser = require ('body-parser');
 const mongoose = require ('mongoose');
 const loginPage = require('./routes/login');
+const users = require('./routes/users');
 const home = require('./routes/home');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/bidit', {useNewUrlParser: true, useUnifiedTopology: true});
 
-//app.use(express.json());
+app.use(express.json());
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/login', loginPage);
 app.use('/', home);
+app.use('/users', users );
 
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,32 @@
 const express = require('express');
 const path = require('path');
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const {User, validate} = require('../models/user');
 const router = express.Router();
+
+router.post('/', async(req,res) => {
+
+  const {error} = validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({email: req.body.email});
+  if (user) return res.status(400).send('User already registered.');
+
+  user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  await user.save();
+
+  res.send(_.pick(user, ['id', 'email']));
+});
+
+
+module.exports = router;
